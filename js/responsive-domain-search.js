fdbd507f77b52d7d97732b6a4d2481cc96f4bd34
PlanetGamingGG <jasonnathanaelsjarif@gmail.com>
2018-07-10 13:55:11 +0700

    Push!

---
+++ js/responsive-domain-search.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    if(typeof affCode == 'undefined'){
+        var affCode = '25147';
+    }
+
+    function foreach(t, e) {
+        for (var n = 0; n < t.length; n++)
+            e(t[n])
+    }
+
+    foreach(document.querySelectorAll('iframe[src^="domainsearch.html"]'), function(t) {
+        widget = t.parentElement,
+            form = document.createElement("form"),
+            form.setAttribute("class", "form-inline"),
+            form.setAttribute("target", "_blank"),
+            form.setAttribute("action", "https://ifastnet.com/portal/domainchecker.php?aff="+affCode+""),
+            form.setAttribute("method", "post"),
+            form.innerHTML = '<input type="hidden" name="direct" value="true"><input type="hidden" name="aff" value="'+affCode+'"><div class="form-group" style="margin: 20px; 10px;"><input type="text" class="form-control" name="domain" size="54"></div><div class="form-group" style="margin: 20px; 10px;"><select name="ext" class="form-control"><option>.com</option><option>.net</option><option>.org</option><option>.info</option><option>.cc</option><option>.co.uk</option><option>.biz</option><option>.bz</option><option>.eu</option><option>.mobi</option><option>.us</option><option>.name</option></select></div><button class="btn btn-info">Check Now</button>',
+            col = document.createElement("div"),
+            col.setAttribute("class", "col-sm-12"),
+            col.appendChild(form),
+            row = document.createElement("div"),
+            row.setAttribute("class", "row"),
+            row.appendChild(col),
+            widget.innerHTML = "",
+            widget.appendChild(row)
+    });
+
+});
---
+++ js/responsive-domain-search.md
@@ -0,0 +1,19 @@
+Responsive Domain Search
+
+Used for :
+Making the search form responsive.
+
+Where to put?
+Anywhere
+
+Installation : 
+Create a script tag with src pointing to responsive-domain-search.js or responsive-domain-search.min.js for minified version and place it on the footer ads (Recomended).
+
+Initialization : 
+A variable "affliate code" can be defined to put your own affliate code on the domain search but according to iFastNet, this will not gave you any money.
+
+Variable : 
+	* affCode -> 5 digit affliate code.
+
+If the variable is not declared, an affliate code for my account (PlanetCloud) will be used.
+In case this does actually pay money, the money will go into my account and I will appriciate it as a donation :).
