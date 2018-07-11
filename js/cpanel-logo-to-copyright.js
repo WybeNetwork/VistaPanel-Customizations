10b4cb5572778399e7862485b6e45b5cb51663f9
PlanetGamingGG <jasonnathanaelsjarif@gmail.com>
2018-07-11 19:14:36 +0700

    Added VPT Prefix

--- js/cpanel-logo-to-copyright.js
+++ js/cpanel-logo-to-copyright.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	/* cPanel Logo to Copyright */	
+	/* cPanel Logo to Copyright */
 
-	if(typeof companyLogo !== 'undefined' && typeof companyName !== 'undefined' && typeof companyStarted !== 'undefined'){
+	if(typeof VPT.companyLogo !== 'undefined' && typeof VPT.companyName !== 'undefined' && typeof VPT.companyStarted !== 'undefined'){
 
-		var cpLogoA = document.getElementById("lnkPoweredByCpanel");
-		var cpLogoS = document.getElementById("txtCpanelVersion");
+		var VPT_TEMP.cpLogoA = document.getElementById("lnkPoweredByCpanel");
+		var VPT_TEMP.cpLogoS = document.getElementById("txtCpanelVersion");
 
-		var cpDate = new Date();
-		var cpYear = cpDate.getFullYear();
+		var VPT_TEMP.cpDate = new Date();
+		var VPT_TEMP.cpYear = cpDate.getFullYear();
 
-		companyStarted = Number(companyStarted);
+		VPT.companyStarted = Number(VPT.companyStarted);
 
-		cpLogoA.href = primarySite;
-		cpLogoA.target = '_blank';
-		cpLogoA.title = companyName;
+		VPT_TEMP.cpLogoA.href = primarySite;
+		VPT_TEMP.cpLogoA.target = '_blank';
+		VPT_TEMP.cpLogoA.title = VPT.companyName;
 
-		cpLogoA.innerHTML = '<img id="imgPoweredByCpanel" width="200px" src="'+companyLogo+'" alt="'+companyName+'" style="display: inline-block; z-index: 999;">';
+		VPT_TEMP.cpLogoA.innerHTML = '<img id="imgPoweredByCpanel" width="200px" src="'+VPT.companyLogo+'" alt="'+VPT.companyName+'" style="display: inline-block; z-index: 999;">';
 
-		if(companyStarted < cpYear){
-			cpLogoS.innerHTML = '© '+companyStarted+'-'+cpYear+' '+companyName+'.';
+		if(VPT.companyStarted < cpYear){
+			VPT_TEMP.cpLogoS.innerHTML = '© '+VPT.companyStarted+'-'+cpYear+' '+VPT.companyName+'.';
 		}else{
-			cpLogoS.innerHTML = '© '+cpYear+' '+companyName+'.';
+			VPT_TEMP.cpLogoS.innerHTML = '© '+cpYear+' '+VPT.companyName+'.';
 		}
 
 	}
