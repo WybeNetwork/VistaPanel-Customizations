06acd97c963303ab62c10675d5691b9a02941348
PCTipsGR <pctipsgr@tuta.io>
2018-09-09 20:51:53 +0300

    Fix Typos

--- change-language-link-text-changer/change-language-link-text-changer.js
+++ change-language-link-text-changer/change-language-link-text-changer.js
@@ -1,13 +1,13 @@
 /* 	**************************************************************************
     	*
-	*   Vista Panel Add-on
+	*   VistaPanel Add-on
 	*   Script Name: "Change Password" Link and Text Changer
-	*   Author: MAHOfficial & VPT (Vista Panel Themes) Team
+	*   Author: MAHOfficial & VPT (VistaPanel Team)
 	*   Author URL: https://mahofficial.win
 	*   Version: 1.0
 	*   Github Project URL: https://github.com/VPTOfficial/VistaPanel-Customizations
 	*   Official Website URL: https://vpthemes.win
-	*   This file is licensed under MIT
+	*   This file is licensed under MIT Attribution
 	*
 	************************************************************************** */
 document.getElementById("lnkUserPrefChangeLang").setAttribute("href", change_language_link);
