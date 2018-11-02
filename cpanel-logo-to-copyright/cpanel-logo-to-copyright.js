7fad23165da36cf623f108d9ae6a068344716ddd
PlanetCloud <35788716+PlanetGamingGG@users.noreply.github.com>
2018-11-02 09:25:36 +0700

    Saving some byet's

--- cpanel-logo-to-copyright/cpanel-logo-to-copyright.js
+++ cpanel-logo-to-copyright/cpanel-logo-to-copyright.js
@@ -1,34 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+    if ("undefined" !== typeof b) {
+        var a = document.getElementById("lnkPoweredByCpanel"),
+            d = document.getElementById("txtCpanelVersion"),
+            c = (new Date).getFullYear();
+        b.company_started = Number(b.company_started);
+        a.href = b.prm_site;
+        a.target = "_blank";
+        a.title = b.company_name;
+        a.innerHTML = '<img id="imgPoweredByCpanel" src="/panel/pl-res/powered_by_cpanel.png" alt="cPanel, Inc." style="display:none;"><img id="imgPoweredByCpanel" src="' + b.company_logo + '" alt="' + b.company_name +
+            '" style="display:inline-block; z-index:2147483647; visibility:visible;">';
+        d.innerHTML = b.company_started < c ? "\u00a9 " + b.company_started + "-" + c + " " + b.company_name + "." : "\u00a9 " + c + " " + b.company_name + "."
+    }
 
-	/* cPanel Logo to Copyright by PlanetCloud for VPT */
+    /* 
+     *	Created at 10 July 2018 by PlanetCloud
+     *	Lase modified at 02 November 2018 by PlanetCloud
+     *	DO NOT REMOVE CREDITS!
+     */
 	
-	if(typeof b !== 'undefined'){
-
-		var a = document.getElementById("lnkPoweredByCpanel");
-		var c = document.getElementById("txtCpanelVersion");
-		var cdate = new Date();
-		var cyear = cdate.getFullYear();
-
-		b.company_started = Number(b.company_started);
-
-		a.href = b.prm_site;
-		a.target = '_blank';
-		a.title = b.company_name;
-
-		a.innerHTML = '<img id="imgPoweredByCpanel" src="/panel/pl-res/powered_by_cpanel.png" alt="cPanel, Inc." style="display:none;"><img id="imgPoweredByCpanel" src="'+b.company_logo+'" alt="'+b.company_name+'" style="display:inline-block; z-index:2147483647; visibility:visible;">';
-
-		if(b.company_started < cyear){
-			c.innerHTML = '© '+b.company_started+'-'+cyear+' '+b.company_name+'.';
-		}else{
-			c.innerHTML = '© '+cyear+' '+b.company_name+'.';
-		}
-
-	}
-
-	/* 
-	 *	Created at 10 July 2018 by PlanetCloud
-	 *	Lase modified at 26 August 2018 by PlanetCloud
-	 *	DO NOT REMOVE CREDITS!
-	 *
-	 */
 });
