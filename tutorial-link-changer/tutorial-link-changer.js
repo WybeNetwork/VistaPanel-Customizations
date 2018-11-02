dfc4a37620b96b08c1c174d55f628fdb8424f90f
PlanetCloud <35788716+PlanetGamingGG@users.noreply.github.com>
2018-11-02 09:28:10 +0700

    Saving some byets

--- tutorial-link-changer/tutorial-link-changer.js
+++ tutorial-link-changer/tutorial-link-changer.js
@@ -1,18 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-    /* Tutorial Links Replacer by PlanetCloud for VPT */
-
-    if(typeof b.tutorial !== 'undefined'){
-        [].forEach.call(document.querySelectorAll('a[href^="http://tutorials.securesignup.net"]'), function(a){
-            a.href = b.tutorial;
-        });
-    }
-
+    "undefined" !== typeof b.tutorial && [].forEach.call(document.querySelectorAll('a[href^="http://tutorials.securesignup.net"]'), function(a) {
+        a.href = b.tutorial
+    })
+    
     /* 
      *  Created at 2 September 2018 by PlanetCloud
-     *  Lase modified at 2 September 2018 by PlanetCloud
+     *  Lase modified at 2 November 2018 by PlanetCloud
      *  DO NOT REMOVE CREDITS!
      *
      */
-
 });
