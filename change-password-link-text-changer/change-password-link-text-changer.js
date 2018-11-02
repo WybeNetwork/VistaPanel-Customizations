7de2109b7a4630071312302cdfdb9a999b3a0158
PlanetCloud <35788716+PlanetGamingGG@users.noreply.github.com>
2018-11-02 09:33:01 +0700

    Saving some byets
    Modified the code to implement "DNRY"

--- change-password-link-text-changer/change-password-link-text-changer.js
+++ change-password-link-text-changer/change-password-link-text-changer.js
@@ -1,14 +1,15 @@
-/* 	**************************************************************************
-    	*
-	*   Vista Panel Add-on
-	*   Script Name: "Change Password" Link and Text Changer
-	*   Author: MAHOfficial & VPT (Vista Panel Themes) Team
-	*   Author URL: https://mahofficial.win
-	*   Version: 1.0
-	*   Github Project URL: https://github.com/VPTOfficial/VistaPanel-Customizations
-	*   Official Website URL: https://vpthemes.win
-	*   This file is licensed under MIT
-	*
-	************************************************************************** */
-document.getElementById("lnkUserPrefChangePwd").setAttribute("href", change_password_link);
-document.getElementById("lnkUserPrefChangePwd").innerHTML=change_password_text;
+/**
+ *	Vista Panel Add-on
+ *	Script Name: "Change Password" Link and Text Changer
+ *	Author: MAHOfficial & VPT (Vista Panel Themes) Team
+ *	Author URL: https://mahofficial.win
+ *	Version: 1.0
+ *	Github Project URL: https://github.com/VPTOfficial/VistaPanel-Customizations
+ *	Official Website URL: https://vpthemes.win
+ *	This file is licensed under MIT
+ *
+ *	Modified at 2 November 2018 by PlanetCloud
+ */
+var n = document.getElementById("lnkUserPrefChangePwd");
+n.setAttribute("href", change_password_link);
+n.innerHTML = change_password_text;
