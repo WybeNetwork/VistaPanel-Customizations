18a4ceb226b0fbea484a4f5e7011bbffcdf96449
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2020-08-06 05:31:44 +0700

    Fix #8

--- tutorial-link-changer/tutorial-link-changer.js
+++ tutorial-link-changer/tutorial-link-changer.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    "undefined" !== typeof b.tutorial && [].forEach.call(document.querySelectorAll('a[href^="http://tutorials.securesignup.net"]'), function(a) {
-        a.href = b.tutorial
-    })
+    document.querySelectorAll('a[href^="http://tutorials.securesignup.net"]').forEach(function(element) {
+        element.setAttribute('href', b.tutorial);
+    });
     
     /* 
      *  Created at 2 September 2018 by PlanetCloud
-     *  Lase modified at 2 November 2018 by PlanetCloud
+     *  Lase modified at 6 August 2020 by PlanetCloud
      *  DO NOT REMOVE CREDITS!
      *
      */
