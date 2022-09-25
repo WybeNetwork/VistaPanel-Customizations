ec8e32a02c40866c4363157e30a79a355321d992
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2022-09-25 13:58:20 +0300

    Element Changer: Add Elements

--- element-changer/element-changer.js
+++ element-changer/element-changer.js
@@ -1,27 +1,33 @@
 /* @preserve
 *  Created at 15 September 2022 by Anyx and modified by PlanetCloud.
-*  Last modified at 19 September 2022 by PlanetCloud.
+*  Modified at 19 September 2022 by PlanetCloud.
+*  Last modified at 25 September 2022 by PlanetCloud.
 *  DO NOT REMOVE CREDITS!
 *  Created for: Wybe Network.
 */
-function indexPanelElements() {
-    let panelElementsIndex = [],
+function indexAppGroups() {
+    let elementsIndex = [],
+        groupsIndex = [],
         g = i = 0;
     PAGE.appGroups.forEach(group => {
         group.items.forEach(item => {
-            if (typeof panelElementsIndex[item.itemdesc] == 'undefined') {
-                panelElementsIndex[item.itemdesc] = [];
+            if (typeof elementsIndex[item.itemdesc] == 'undefined') {
+                elementsIndex[item.itemdesc] = [];
             }
-            panelElementsIndex[item.itemdesc].push({
+            elementsIndex[item.itemdesc].push({
                 group: g,
                 item: i
             });
             i++;
         });
+        groupsIndex[group.group] = g;
         g++;
         i = 0;
     });
-    return panelElementsIndex;
+    return {
+        panelElements: elementsIndex,
+        groups: groupsIndex
+    };
 }
 
 (() => {
@@ -34,14 +40,16 @@ function indexPanelElements() {
 
     // Index changeElements
     var toChange = {
+        // Remove first, then modify, then add
+        remove: [],
         modify: [],
-        remove: []
+        add: []
     };
     changeElements.forEach(e => {
         if (typeof e.action == 'undefined') {
             e.action = 'modify';
         }
-        if (!['modify', 'remove'].includes(e.action)) {
+        if (!['modify', 'remove', 'add'].includes(e.action)) {
             console.log(`ElementChanger: Illegal action ${e.action} specified for the following element:`, e);
             return;
         }
@@ -50,28 +58,75 @@ function indexPanelElements() {
 
     // Remove elements
     if (toChange.remove.length > 0) {
-        panelElementsIndex = indexPanelElements();
+        let elementsIndex = indexAppGroups().panelElements;
         toChange.remove.forEach(r => {
-            if (typeof panelElementsIndex[r.name] == 'undefined') {
-                return console.log(`ElementChanger: Trying to remove element ${r.name} which does not exist.`)
+            if (typeof elementsIndex[r.name] == 'undefined') {
+                return console.log(`ElementChanger: Trying to remove element "${r.name}" which does not exist.`)
             }
-            panelElementsIndex[r.name].forEach(e => {
+            elementsIndex[r.name].forEach(e => {
                 PAGE.appGroups[e.group].items = PAGE.appGroups[e.group].items.filter(i => i.itemdesc != r.name);
             });
         });
     }
 
     // Modify elements
-    panelElementsIndex = indexPanelElements();
+    let elementsIndex = indexAppGroups().panelElements;
     toChange.modify.forEach(c => {
-        if (typeof panelElementsIndex[c.name] == 'undefined') {
-            return console.log(`ElementChanger: Trying to change element ${c.name} which does not exist.`);
+        if (typeof elementsIndex[c.name] == 'undefined') {
+            return console.log(`ElementChanger: Trying to change element "${c.name}" which does not exist.`);
         }
-        panelElementsIndex[c.name].forEach(e => {
+        elementsIndex[c.name].forEach(e => {
             PAGE.appGroups[e.group].items[e.item][c.attr] = c.value;
         })
     });
 
+    // Add elements
+    let groupsIndex = indexAppGroups().groups;
+    var addedElementsIcon = [];
+    toChange.add.forEach(a => {
+        if (typeof groupsIndex[a.group] == 'undefined') {
+            return console.log(`ElementChanger: Trying to add an element to "${a.group}" group which does not exist.`)
+        }
+        let file_id = `added_${a.name.replace(/\s+/g, '_')}${Math.floor(Math.random() * 100)}`;
+        PAGE.appGroups[groupsIndex[a.group]].items.push({
+            "file": file_id,
+            "imgtype": "icon",
+            "group": a.group,
+            "itemdesc": a.name,
+            "url": a.link,
+            "type": "image",
+            "height": "48",
+            "width": "48",
+            "searchtext": a.search_text ?? ""
+        });
+        addedElementsIcon.push({
+            target: `icon-${file_id}`,
+            icon: a.icon
+        });
+    });
+
+    // Add icons to added elements
+    function fixIcons() {
+        if (document.getElementById('preferences-group') === null) {
+            return setTimeout(() => {
+                fixIcons();
+            }, 100);
+        }
+        if (addedElementsIcon.length < 1) {
+            return;
+        }
+        console.log('ElementChanger: Fixing icons');
+        addedElementsIcon.forEach(e => {
+            document.getElementById(e.target).innerHTML = `<img class="itemImageWrapper integrations_icon spriteicon_img" src="${e.icon}"/>`
+        });
+        return console.log('ElementChanger: All modifications have been made.');
+    }
+    document.addEventListener("DOMContentLoaded", () => {
+        fixIcons();
+    });
+
     // Done!
-    console.log('ElementChanger: All modifications have been made.');
+    if (addedElementsIcon.length < 1) {
+        return console.log('ElementChanger: All modifications have been made.');
+    }
 })();
